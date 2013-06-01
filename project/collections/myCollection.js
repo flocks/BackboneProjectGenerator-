698a7b5fb25a8a9d6402c4e3879a6ae2be0b572a
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-01 17:40:07 +0200

    hey

---
+++ project/collections/myCollection.js
@@ -0,0 +1,5 @@
+define(function() {
+	return Parse.Collection.extend({
+	
+	});
+});
---
+++ project/models/myModel.js
@@ -0,0 +1,9 @@
+define(function() {
+	return Parse.Object.extend({
+		initialize : function() {
+			
+		},
+  		className: "myModel"
+	});
+ 
+});
