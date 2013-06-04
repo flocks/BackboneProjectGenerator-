a4c57a30e624971d7d26cf40cd34cdadde961de2
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-04 23:46:49 +0200

    add repo creation

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ var project = process.argv[2];
 
 var pathBase = '/Users/flocks';
 var ncp = require('ncp').ncp;
+var GitHubApi = require("node-github");
+
+var github = new GitHubApi({
+    // required
+    version: "3.0.0",
+    // optional
+    timeout: 5000
+});
+
+github.authenticate({
+    type: "oauth",
+    token: "53916f4092bc617bcb6a691adbef54a6276dc27f"
+});
+
+
 
 ncp.limit = 16;
 
@@ -18,6 +33,9 @@ fs.mkdir(pathBase+'/'+project,function(e){
    				return console.error(err);
  			}
 			 console.log('done!');
+	     github.repos.create({name : project, private : true}, function(err) {
+		if(err) { console.log(err);}
+	     })
 		});
 
     } else {
